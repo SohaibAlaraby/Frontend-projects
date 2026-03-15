2d5d74152933e1fc48bca14287fcbc8a3fc83e6d
SohaibAlaraby <110909784+SohaibAlaraby@users.noreply.github.com>
2026-03-15 23:33:57 +0200

    Add files via upload
    Latest code update

--- Weather_App/Weather_App.js
+++ Weather_App/Weather_App.js
@@ -7,24 +7,73 @@ const SearchBarContainer = document.getElementById("SearchIn-SearchBtn");
 const APIKey = "1ebc6bcbf4534de884f150541260203";
 const baseURL = "https://api.weatherapi.com/v1/forecast.json?"
 let WeatherData;
-window.addEventListener('load', async function(event) {
-    await loadWeatherData('Alexandria');
-    document.getElementById('loader').classList.add('loader-hidden');
-    const WeatherContainer = document.getElementById("WeatherAppContainer");
-    WeatherContainer.classList.add("ShowWeatherAppContainer");
-});
+let searchController;
+class WeatherError extends Error {
+    constructor(name,message) {
+        super(message);
+        this.name = name;
+    }
+}
+const TryAgainbtn = document.getElementById('TryAgain');
+window.addEventListener('load', loadInitialData);
+TryAgainbtn.addEventListener('click',loadInitialData);
 SearchBtn.addEventListener("click",searchBtnPressed);
 CelBtn.addEventListener("click",(event)=>{
-    updateTempAndWeatherCondition(true);
-    updateExtraWeatherInfo(true);
+    updateTempAndWeatherCondition(WeatherData.current,true);
+    updateExtraWeatherInfo(WeatherData, true);
+    createHourlyBar(WeatherData.forecast,true);
+    createDaysBar(WeatherData.forecast, true);
 });
 FahBtn.addEventListener("click",(event)=>{
-    updateTempAndWeatherCondition(false);
-    updateExtraWeatherInfo(false);
+    updateTempAndWeatherCondition(WeatherData.current,false);
+    updateExtraWeatherInfo(WeatherData, false);
+    createHourlyBar(WeatherData.forecast,false);
+    createDaysBar(WeatherData.forecast, false);
 });
 SearchInput.addEventListener("input",handleInput);
 
+async function loadInitialData(event){
+    const WeatherContainer = document.getElementById("WeatherAppContainer");
+    const loader = document.getElementById('loader');
+    const spinner = document.getElementById('spinner');
+    const delay = (ms) => new Promise((resolve) => setTimeout(resolve,ms));
+    spinner.classList.remove("hidden");
+    TryAgainbtn.classList.add("hidden");
+    let previousMessage = loader.querySelector('p');
+    if(previousMessage){
+        previousMessage.remove();
+    }
 
+    try {
+        let data;
+        let i = 0;
+        while(true){
+            try{
+                data = await loadWeatherData('Alexandria');
+            }catch{
+                await delay(500);
+            }
+            
+            if(data || i > 2) break;
+            i++;
+        }
+        if(!data) throw new Error("Check Your Internet Connection");
+            
+        WeatherData = data;
+        console.log(WeatherData);
+        updateWeatherDataInUI(data);
+        loader.classList.add('hidden');
+        WeatherContainer.classList.remove("hidden");
+    }catch(error){
+        const WarningMessage = document.createElement('p');
+        WarningMessage.textContent = error.message;
+        WarningMessage.style.color = "red";
+        spinner.classList.add("hidden");
+        loader.prepend(WarningMessage);
+        TryAgainbtn.classList.remove("hidden");
+
+    }
+}
 function handleInput(event) {
     let value = event.target.value.trim();
     if(!event.target.value || validateCityName(value)) deleteWarningMessage();
@@ -70,14 +119,12 @@ function getWeatherGroup(code) {
     return 'sunny';
 }
 
-function updateBackground() {
-    const code = WeatherData.current.condition.code;
+function updateBackground({condition:{code}, is_day}) {
     const groupName = getWeatherGroup(code);
     const body = document.body;
     body.classList.remove('bg-sunny','bg-clear','bg-cloudy','bg-mist','bg-rainy','bg-snowy','bg-thunder');
     if(groupName === 'sunny') {
-        let isDay = WeatherData.current.is_day;
-        if(!isDay){
+        if(!is_day){
             body.classList.add(`bg-clear`);
             return;
         }
@@ -85,31 +132,27 @@ function updateBackground() {
     body.classList.add(`bg-${groupName}`);
 }
 
-function updateWeatherIcon() {
+function updateWeatherIcon({is_day, condition:{code,text:WeatherCondition}}) {
     const weatherIconUI = document.getElementById('WeatherIcon');
     const baseURL = './assets/Icons/';
-    const code = WeatherData.current.condition.code;
     const groupName = getWeatherGroup(code);
-    const isDay = WeatherData.current.is_day;
-
+    weatherIconUI.alt = WeatherCondition;
     if(groupName === 'sunny'){
-        weatherIconUI.src = isDay? `${baseURL}sunny.png`:`${baseURL}clear.png`;
+        weatherIconUI.src = is_day? `${baseURL}sunny.png`:`${baseURL}clear.png`;
     } else if(groupName === 'cloudy') {
-        weatherIconUI.src = isDay?`${baseURL}CloudDay.png`:`${baseURL}CloudNight.png`;
+        weatherIconUI.src = is_day?`${baseURL}CloudDay.png`:`${baseURL}CloudNight.png`;
     }else{
         weatherIconUI.src = `${baseURL}${groupName}.png`;
     }
 
 }
-function updateCityAndCountryNames() {
-    const [city, country] = [WeatherData.location.name, WeatherData.location.country];
+function updateCityAndCountryNames({name:city, country}) {
     const cityUI = document.getElementById("City");
     cityUI.textContent = city;
     const countryUI = document.getElementById("Country");
     countryUI.textContent = country;
 }
-function updateDateAndTime() {
-    const time = WeatherData.location.localtime;
+function updateDateAndTime({localtime:time}) {
     const Months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
     const Days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
@@ -121,43 +164,31 @@ function updateDateAndTime() {
     const month = Months[localtime.getMonth()]; 
     DateUI.textContent = `${day}, ${localtime.getDate()} ${month} ${localtime.getFullYear()}`;
     const TimeUI = DateTimeUI.querySelector('#Time');
-    const hour = localtime.getHours();
-    const minute = localtime.getMinutes();
-    let newMinute = (minute >= 10)? `${minute}` : `0${minute}`;
-    let newHour = (hour%12 >=10)? `${hour%12}`: `0${hour%12}`;
-    if(hour > 12) {
-        TimeUI.textContent = `${newHour}:${newMinute} PM`;
-    } else if(hour < 12) {
-        if(hour === 0) {
-            newHour = `12`;
-        }
-        TimeUI.textContent = `${newHour}:${newMinute} AM`;
-    } else {
-        TimeUI.textContent = `${hour}:${newMinute} PM`;
-    }
+    TimeUI.textContent = localtime.toLocaleTimeString('en-US',{
+        hour12:true,
+        hour:"2-digit",
+        minute:"2-digit"
+    }); 
 }
-function updateTempAndWeatherCondition(isCel) {
+function updateTempAndWeatherCondition({temp_c, temp_f,condition:{text:WeatherText}},isCel) {
     const TempWeatherConditionContainer = document.getElementById('Temp-Units-WeatherCondition');
     const TempUI = TempWeatherConditionContainer.querySelector('#Temp');
     const WeatherUI = TempWeatherConditionContainer.querySelector('#WeatherCondition');
     const FahBtnUI = TempWeatherConditionContainer.querySelector('#FahrenheitBtn');
     const CelBtnUI = TempWeatherConditionContainer.querySelector('#CelciusBtn');
     if(!isCel) {
-        const currentTemp= WeatherData.current.temp_f;
-        TempUI.textContent = currentTemp;
+        TempUI.textContent = temp_f;
         FahBtnUI.classList.remove('deactive-unit');
         CelBtnUI.classList.add('deactive-unit');
         
     } else {
-        const currentTemp= WeatherData.current.temp_c;
-        TempUI.textContent = currentTemp;
+        TempUI.textContent = temp_c;
         FahBtnUI.classList.add('deactive-unit');
         CelBtnUI.classList.remove('deactive-unit');
     }
-    const currentWeather = WeatherData.current.condition.text;
-    WeatherUI.textContent = currentWeather; 
+    WeatherUI.textContent = WeatherText; 
 }
-function updateExtraWeatherInfo(isCel) {
+function updateExtraWeatherInfo({current:{feelslike_c,feelslike_f,wind_kph,wind_mph,humidity,wind_dir}, forecast:{forecastday:[{day:{maxtemp_c,mintemp_c,maxtemp_f,mintemp_f}}]}},isCel) {
     const feelsLikeUI = document.getElementById('FeelsLikeTemp');
     const maxTempUI = document.getElementById('MaxTemp');
     const minTempUI = document.getElementById('MinTemp');
@@ -165,33 +196,65 @@ function updateExtraWeatherInfo(isCel) {
     const windUI = document.getElementById('WindSpeed');
     const winddirUI = document.getElementById('windDirection');
     if(!isCel){
-        feelsLikeUI.textContent = `${WeatherData.current.feelslike_f}°F`;
-        maxTempUI.textContent = WeatherData.forecast.forecastday[0].day.maxtemp_f;
-        minTempUI.textContent = WeatherData.forecast.forecastday[0].day.mintemp_f;
-        windUI.textContent = `${WeatherData.current.wind_mph} Mile/h`;
+        feelsLikeUI.textContent = `${feelslike_f}°F`;
+        maxTempUI.textContent = maxtemp_f;
+        minTempUI.textContent = mintemp_f;
+        windUI.textContent = `${wind_mph} Mile/h`;
         
     } else {
-        feelsLikeUI.textContent = `${WeatherData.current.feelslike_c}°C`;
-        maxTempUI.textContent = WeatherData.forecast.forecastday[0].day.maxtemp_c;
-        minTempUI.textContent = WeatherData.forecast.forecastday[0].day.mintemp_c;
-        windUI.textContent = `${WeatherData.current.wind_kph} Km/h`;
+        feelsLikeUI.textContent = `${feelslike_c}°C`;
+        maxTempUI.textContent = maxtemp_c;
+        minTempUI.textContent = mintemp_c;
+        windUI.textContent = `${wind_kph} Km/h`;
     }
-    humidityUI.textContent = WeatherData.current.humidity;
+    humidityUI.textContent = humidity;
     
-    winddirUI.textContent=WeatherData.current.wind_dir;
+    winddirUI.textContent = wind_dir;
 
 }
-async function loadWeatherData(cityName){
-    WeatherData = await getWeatherData(cityName);
-    if(!WeatherData) return;
-    updateCityAndCountryNames();
-    updateDateAndTime();
-    updateTempAndWeatherCondition(true);
-    updateExtraWeatherInfo(true);
-    updateWeatherIcon()
-    updateBackground();
-    console.log(WeatherData);
 
+async function loadWeatherData(cityName){
+    try{
+        /*
+        if user fetch multiple times abort all previous fetches
+        and keep only the newest one
+        */
+        if(!cityName || !validateCityName(cityName)) return;
+        const url = prepareURL(cityName,baseURL);
+        const {signal,TimeoutID} = prepareNewRequest();
+        const response = await fetch(url,{signal});
+        clearTimeout(TimeoutID);
+        if(!response.ok) {
+            const errorData = await response.json();
+            throw new Error(errorData.error.message||"Something wents wrong");
+        }
+        const data = await response.json();
+        return data;
+    } catch(error) {
+        if(error.name ==='AbortError') {
+            console.log('Previous request is cancelled');
+        } else  {
+            throw error;
+        }
+    }
+    
+
+}
+function updateWeatherDataInUI(data){
+
+    try{
+        updateCityAndCountryNames(data.location);
+        updateDateAndTime(data.location);
+        updateTempAndWeatherCondition(data.current, true);
+        updateExtraWeatherInfo(data, true);
+        updateWeatherIcon(data.current);
+        updateBackground(data.current);
+        createHourlyBar(data.forecast,true);
+        createDaysBar(data.forecast, true);
+        updateUVIndex(data);
+    }catch(error) {
+        console.error("UI update error:",error);
+    }
 }
 async function searchBtnPressed(event) {
     event.preventDefault();
@@ -202,35 +265,111 @@ async function searchBtnPressed(event) {
         return;
     }
     deleteWarningMessage();
+    let data;
+    try{
+        data = await loadWeatherData(SearchInputContent);
+        if(data){
+        WeatherData = data;
+        console.log(WeatherData);
+        updateWeatherDataInUI(data);
+        }
+    }catch(error){
+        if(!navigator.onLine || error instanceof TypeError){
+            showWarningMessage("Network error: Please check your connection.");
+        } else  {
+            showWarningMessage(error.message);
+        }
+        return;
+    }
     
-    await loadWeatherData(SearchInputContent);
+    
     
 }
+function prepareNewRequest() {
+    if(searchController) {
+        searchController.abort()
+    }
+    searchController = new AbortController();
+    const TimeoutID = setTimeout(()=>{ 
+    searchController.abort()
+    }, 10000);
+    return {signal:searchController.signal, TimeoutID: TimeoutID};
+}
 
-async function getWeatherData(city) {
-    //fetch the weather from api
-    if(!city || !validateCityName(city)) return null;
-
+function prepareURL(city,baseURL){
     const params = new URLSearchParams({
         key: APIKey,
         q: city,
-        days: 1,//fetch one day until finishing the ui
+        days: 3,//fetch one day until finishing the ui
         aqi: "no"
 
     });
-    const url = `${baseURL}${params}`;
-    try {
-        const response = await fetch(url);
-        if(!response.ok) {
-            const errorData = await response.json();
-            throw new Error(errorData.error.message||"Something wents wrong");
-        }
-        const data = await response.json();
-        return data; 
+    return `${baseURL}${params}`;
+}
+function createHourlyBar({forecastday:[{hour:hourArr}]},isCel){
+    const HourlyBarContainer = document.getElementById("TodayDetailedInfo");
+    HourlyBarContainer.innerHTML = hourArr.map((hour,index) => {
+        let Time = new Date(hour.time).toLocaleTimeString('en-US',{
+        hour12:true,
+        hour:"2-digit"
+    });
 
-    } catch (error) {
-        showWarningMessage(error.message);
-        return null;
-    }
+        return `
+        <div class="HourByHourInfo" data-hour-index =${index} >
+            <span>${Time}</span>
+            <img src="https://${hour.condition.icon}" alt="${hour.condition.text}">
+            <span>${isCel?hour.temp_c:hour.temp_f}°</span>
+            <span><i class="fa-solid fa-droplet"></i> ${hour.humidity}%</span>
+        </div>
+        `
+    }).join('');
 }
 
+function createDaysBar({forecastday}, isCel) {
+    const DaysBarContainer = document.getElementById('NextDaysPrediction');
+    const Days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    DaysBarContainer.innerHTML = forecastday.map((day,index) => {
+        let dateObj= new Date(day.date);
+        let dayName = index === 0? "Today" : Days[dateObj.getDay()];
+        let [maxtemp, mintemp] = isCel?[day.day.maxtemp_c, day.day.mintemp_c]:[day.day.maxtemp_f, day.day.mintemp_f];
+        let humidity = day.day.avghumidity;
+        return `
+        <div class="DayByDayInfo" data-day-index =${index} >
+            <span>${dayName}</span>
+            <img src="https://${day.day.condition.icon}" alt="${day.day.condition.text}">
+            <span><i class="fa-solid fa-arrow-down-long fa-flip-vertical"></i> ${maxtemp}° / <i class="fa-solid fa-arrow-down-long"></i> ${mintemp}°</span>
+            <span><i class="fa-solid fa-droplet"></i> ${humidity}%</span>
+        </div>
+        ` 
+    }).join('');
+
+}
+
+function updateUVIndex({current:{uv:UVIndex}}){
+    //{current:{uv:UVIndex}}
+    const UVdescription = document.getElementById("UVIndexDescribtion");
+    const UVPointer = document.getElementById("UVPointer");
+    UVPointer.textContent = UVIndex;
+    let percentage = (UVIndex/11)*100;
+    percentage = percentage>100? 100 : percentage;   
+    UVPointer.style.left = `${percentage}%`;
+    if(0 <= UVIndex && 2 >= UVIndex) {
+        UVdescription.textContent = "low";
+        UVPointer.style.backgroundColor = '#289500';
+    } else if(3 <= UVIndex && 5 >= UVIndex) {
+        UVdescription.textContent = "moderate";
+        UVPointer.style.backgroundColor = '#f7e400';
+    } else if(6 <= UVIndex && 7 >= UVIndex) {
+        UVdescription.textContent = "high";
+        UVPointer.style.backgroundColor = '#f85900';
+    } else if(8 <= UVIndex && 10 >= UVIndex) {
+        UVdescription.textContent = "very high";
+        UVPointer.style.backgroundColor = '#d8001d';
+    } else if(11 <= UVIndex) {
+        UVdescription.textContent = "extreme";
+        UVPointer.style.backgroundColor = '#6b49c8';
+    } else {
+        const UVIndexContainer= document.getElementById("UVIndex");
+        UVIndexContainer.style.display = "none";
+    }
+}
